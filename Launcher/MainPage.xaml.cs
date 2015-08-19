f1966caf5ae87356c86e2395f27e69461fe9318f
Dave Glover <dglover@microsoft.com>
2015-08-19 16:35:07 +1000

    Revert "refactor"
    This reverts commit b98489b330cda60a21ebb86318aa1a5ffec8016d.

---
+++ Launcher/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Glovebox.IO.VirtualArduino.Actuators;
+using Glovebox.IO.VirtualArduino.Sensors;
+using Glovebox.IoT;
+using Microsoft.Maker.RemoteWiring;
+using Microsoft.Maker.Serial;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// Arduino Uno - A0 on Pin 14
+// http://www.microsofttranslator.com/bv.aspx?from=&to=en&a=http%3A%2F%2Fblogs.msdn.com%2Fb%2Fsos%2Farchive%2F2015%2F07%2F11%2Fwindows-remote-arduino-creating-lamp-controlled-by-universal-windows-app.aspx
+
+namespace RemoteArduino {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page {
+        UsbSerial connection;
+        RemoteDevice arduino;
+
+        DispatcherTimer dt;
+
+        VirtualRelay relay;
+        VirtualLed led;
+        VirtualLDR ldr;
+
+        byte relay_pin = 7;
+        byte led_pin = 3;
+        VirtualLDR.UnoAnalogPins ldr_pin = VirtualLDR.UnoAnalogPins.A0;
+
+        bool auto_mode = false;
+
+        public MainPage() {
+            this.InitializeComponent();
+            connect();
+
+            Util.StartNetworkServices(true);
+        }
+
+        private void connect() {
+            dt = new DispatcherTimer() { Interval = new TimeSpan(500) };
+            dt.Tick += loop;
+
+            //useful when running on Raspberry Pi 2 on Windows 10 for IoT and getting the vid and pid not so easy
+            //var dev = await UsbSerial.listAvailableDevicesAsync();
+            //usbcomm = new UsbSerial(dev[0]);
+
+            connection = new UsbSerial("VID_2341", "PID_0043");
+
+            arduino = new RemoteDevice(connection);
+            connection.ConnectionEstablished += Connection_ConnectionEstablished;
+            connection.begin(250000, SerialConfig.SERIAL_8N1);
+        }
+
+        private void Connection_ConnectionEstablished() {
+            var action = Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, new Windows.UI.Core.DispatchedHandler(() => {
+
+                relay = new VirtualRelay(arduino, relay_pin, "relay01");
+                led = new VirtualLed(arduino, led_pin, "led01");
+                ldr = new VirtualLDR(arduino, ldr_pin, Timeout.Infinite, "light01");
+
+                dt.Start();
+
+                on.IsEnabled = true;
+                off.IsEnabled = true;
+                auto.IsEnabled = true;
+               // auto_mode = true;
+            }));
+        }
+
+        private void loop(object sender, object e) {
+            if (auto_mode) {
+                if (ldr.Current < 512) {
+                    relay.On();
+                    led.On();
+                }
+                else {
+                    relay.Off();
+                    led.Off();
+                }
+            }
+        }
+
+        private void on_Click(object sender, RoutedEventArgs e) {
+            auto_mode = false;
+            relay.On();
+            led.On();
+        }
+
+        private void off_Click(object sender, RoutedEventArgs e) {
+            auto_mode = false;
+            relay.Off();
+            led.Off();
+        }
+
+        private void auto_Click(object sender, RoutedEventArgs e) {
+            auto_mode = true;
+        }
+    }
+}
